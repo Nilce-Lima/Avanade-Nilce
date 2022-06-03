72d88547476e6007060058e6c8329b16402d84cd
nilce lima <nilcesdl_81@hotmail.com>
2022-06-03 02:36:54 -0300

    exercício adicionado

---
+++ primeiro.js
@@ -0,0 +1,23 @@
+function retornaNome(nome) {
+    return nilce;
+    }
+    
+    function retornaNomeParametro(nome) {
+        return nome;
+    }
+    
+    function divisao(n1, n2) {
+        if (n2 === 0) {
+            return 0;
+        }
+    return n1/n2;
+    }
+    
+    function AlunosPresentes() {
+        return ["Nilce", "André", "Cássia", "Erick", "Felipe","Moisés"];
+    }
+    
+    function adicionarArray(arr, valor) {
+        arr.push(valor);
+        return arr;
+    }
